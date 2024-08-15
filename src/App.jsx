35d80f356f17d17b78d4728991d9ca3d8784552f
JPham4801 <JPham4801@gmail.com>
2024-08-14 20:54:41 -0500

    resolving deployment issues with frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
-import './App.css';
+import * as authService from './services/authService'; // Corrected import path
 import Marketplace from './components/Marketplace/Marketplace';
 import ListingDetails from './components/ListingDetails/ListingDetails';
-
-export const AuthedUserContext = createContext(null);
 import ListingForm from './components/ListingForm/ListingForm';
 
+export const AuthedUserContext = createContext(null);
+
 const App = () => {
-  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [user, setUser] = useState(authService.getUser()); // Initialize user state using authService
 
   const handleSignout = () => {
-    authService.signout();
-    setUser(null);
+    authService.signout(); // Call signout method from authService
+    setUser(null); // Clear user state
   };
+
   return (
     <>
       <AuthedUserContext.Provider value={user}>
@@ -28,9 +28,9 @@ const App = () => {
           {user ? <Route path="/" element={<Dashboard user={user} />} /> : <Route path="/" element={<Landing />} />}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
           <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-          <Route path ='/new' element={<ListingForm />} />
-          <Route path ='/update/:listingId' element={<ListingForm /> } />
-          <Route path ='/Marketplace' element={<Marketplace/>}/>
+          <Route path="/new" element={<ListingForm />} />
+          <Route path="/update/:listingId" element={<ListingForm />} />
+          <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/listings/:id" element={<ListingDetails />} />
         </Routes>
       </AuthedUserContext.Provider>
